6e1a52a093ba876a84a99b11b1e18dd58486b694
Achala Pal <55523845+AchalApal-A@users.noreply.github.com>
2026-05-15 14:52:46 +0530

    Update OnboardingApplication.java

--- src/main/java/com/example/onboarding/OnboardingApplication.java
+++ src/main/java/com/example/onboarding/OnboardingApplication.java
@@ -2,12 +2,12 @@ package com.example.onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class OnboardingApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(OnboardingApplication.class, args);
     }
-
 }
