b74f27266cedee05033d468d9324c4f4b3067a21
Achala Pal <55523845+AchalApal-A@users.noreply.github.com>
2026-05-15 14:30:20 +0530

    Update EmailService.java

--- src/main/java/com/example/onboarding/service/EmailService.java
+++ src/main/java/com/example/onboarding/service/EmailService.java
@@ -49,9 +49,10 @@ public class EmailService {
 
             mailSender.send(message);
 
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to send email: " + e.getMessage());
-        }
+        }catch (Exception e) {
+    e.printStackTrace();
+    throw new RuntimeException("Failed to send email", e);
+}
     }
 
     // ✅ Generate UTM
